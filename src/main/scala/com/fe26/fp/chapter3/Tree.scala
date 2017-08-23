6a897940215f22a34c3da19d4f4455d6e6c64a8c
Bilal Wahla <bilalwahla@gmail.com>
2017-08-23 19:00:51 +0100

    Labelled exercises.

--- src/main/scala/com/fe26/fp/chapter3/Tree.scala
+++ src/main/scala/com/fe26/fp/chapter3/Tree.scala
@@ -7,6 +7,7 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
+  /* Exercise 3.25 */
   def size[A](t: Tree[A]): Int = t match {
     case Leaf(_) => 1
     case Branch(l, r) => 1 + size(l) + size(r)
@@ -14,6 +15,7 @@ object Tree {
 
   def size2[A](t: Tree[A]): Int = fold(t)(_ => 1)((l, r) => 1 + l + r)
 
+  /* Exercise 3.26 */
   def maximum(t: Tree[Int]): Int = t match {
     case Leaf(v) => v
     case Branch(l, r) => maximum(l) max maximum(r)
@@ -21,6 +23,7 @@ object Tree {
 
   def maximum2(t: Tree[Int]): Int = fold(t)(a => a)((l, r) => l max r)
 
+  /* Exercise 3.27 */
   def depth[A](t: Tree[A]): Int = t match {
     case Leaf(_) => 0
     case Branch(l, r) => 1 + (depth(l) max depth(r))
@@ -28,6 +31,7 @@ object Tree {
 
   def depth2[A](t: Tree[A]): Int = fold(t)(_ => 0)((l, r) => 1 + (l max r))
 
+  /* Exercise 3.28 */
   def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
     case Leaf(v) => Leaf(f(v))
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
